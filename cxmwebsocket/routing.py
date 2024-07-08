8cffed0677ace2a8d145b03891eba994a992d2c3
Kaan <kaan.ozmen@ebrandvalue.com>
2024-07-08 17:18:30 +0300

    refactor

--- cxmwebsocket/routing.py
+++ cxmwebsocket/routing.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/hello/', consumers.MyConsumer.as_asgi()),
+    path('ws/redirect/', consumers.Router.as_asgi()),
     path('ws/conversation/', consumers.ConversationConsumer.as_asgi()),
     path('ws/config/', consumers.NotificationConsumer.as_asgi()),
     path('ws/post/', consumers.ConversationRedirectToUser.as_asgi()),
