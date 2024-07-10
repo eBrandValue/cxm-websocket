b499096c7552a0aa4eed4e3e3b5db20e02d85dc7
Kaan <kaan.ozmen@ebrandvalue.com>
2024-07-10 14:32:16 +0300

    test

--- cxm_websocket/asgi.py
+++ cxm_websocket/asgi.py
@@ -1,4 +1,6 @@
 import os
+import django
+django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
